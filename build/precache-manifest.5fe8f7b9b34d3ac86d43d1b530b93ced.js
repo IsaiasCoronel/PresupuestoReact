4fe58c284f9f9d720909385978c8ee703bfe57f4
ejemplo <ejemplo@gmail.com>
2021-05-11 10:22:31 -0500

    first commit

---
+++ build/precache-manifest.5fe8f7b9b34d3ac86d43d1b530b93ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33cdd76d5d16ca873601469f2abb3fd0",
+    "url": "/PresupuestoReact/index.html"
+  },
+  {
+    "revision": "c483702ae769120c59c4",
+    "url": "/PresupuestoReact/static/css/main.61c744dc.chunk.css"
+  },
+  {
+    "revision": "6d40b863712b9d82ee8e",
+    "url": "/PresupuestoReact/static/js/2.1c12240e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/PresupuestoReact/static/js/2.1c12240e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c483702ae769120c59c4",
+    "url": "/PresupuestoReact/static/js/main.54b9d5dc.chunk.js"
+  },
+  {
+    "revision": "8b78e91ec2dd221afeed",
+    "url": "/PresupuestoReact/static/js/runtime-main.f9ec5c27.js"
+  }
+]);
